e25b3ed3a8c457136cf5c0061a51959b37514d31
JS <jstehlik@davincisoftware.sk>
2018-03-28 16:51:04 +0200

    main example

---
+++ cloudmigration_main.py
@@ -0,0 +1,11 @@
+from cloudmigration.Loader import Loader
+from cloudmigration.Translation import Translation, Template
+
+loader = Loader()
+from_template = Template() #create empty template class
+from_template.dictFromFile("orbis-lab.yaml", "YAML") #load template in yaml format to template
+translation = Translation("OpenStack", from_template.template, "AWS", loader) #creata instance of translation
+to_template = Template(translation.translate()) #translate the template
+print(to_template.template)
+to_template.dictToFile("orbis.json", "JSON") #save to JSON
+to_template.dictToFile("to_orbis.yaml", "YAML") #save to Zaml
